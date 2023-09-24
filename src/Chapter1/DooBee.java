5c70d258bdecdace7aab937b3486f9b3167646c0
Rodrigo <rss.999999999@gmail.com>
2023-09-24 12:06:52 -0300

    Initial commit

---
+++ src/Chapter1/DooBee.java
@@ -0,0 +1,13 @@
+public class DooBee {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 0 ) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 1  ) {
+            System.out.print("Do");
+        }
+    }
+}
