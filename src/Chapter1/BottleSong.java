4482e304c361acbd7dd9c64cdf58f6338b373fa8
Rodrigo <rss.999999999@gmail.com>
2023-09-28 13:33:15 -0300

    BottleSong v4

--- src/Chapter1/BottleSong.java
+++ src/Chapter1/BottleSong.java
@@ -1,37 +1,38 @@
-/*There’s still one little flaw in our code.  It compiles and runs, but the output isn’t 100% perfect. See if you can spot the flaw and fix it.*/
-
-
-package Chapter1;
-
-
-public class BottleSong {
-    public static void main(String[] args) {
-        int bottlesNum = 10;
-        String word = "bottles";
-
-        while (bottlesNum > 0) {
-
-            if (bottlesNum == 1) {
-                word = "bottle"; // singular, as in ONE bottle.
-            }
-
-            System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
-            System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
-            System.out.println("And if one green bottle should accidentally fall,");
-            bottlesNum = bottlesNum - 1;
-
-            if  (bottlesNum > 0) {
-                System.out.println("There'll be " + bottlesNum +
-                        " green " + word + ", hanging on the wall");
-            } else {
-                System.out.println("There'll be no green bottles, hanging on the wall");
-            } // end else
-        } // end while loop
-    } // end main method
-} // end class
-
 
 /*********************************************************************
+
+
+ There’s still one little flaw in our code.  It compiles and runs, but the output isn’t 100% perfect. See if you can spot the flaw and fix it.
+
+
+ public class BottleSong {
+ public static void main(String[] args) {
+ int bottlesNum = 10;
+ String word = "bottles";
+
+ while (bottlesNum > 0) {
+
+ if (bottlesNum == 1) {
+ word = "bottle"; // singular, as in ONE bottle.
+ }
+
+ System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+ System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+ System.out.println("And if one green bottle should accidentally fall,");
+ bottlesNum = bottlesNum - 1;
+
+ if  (bottlesNum > 0) {
+ System.out.println("There'll be " + bottlesNum +
+ " green " + word + ", hanging on the wall");
+ } else {
+ System.out.println("There'll be no green bottles, hanging on the wall");
+ } // end else
+ } // end while loop
+ } // end main method
+ } // end class
+ There’s still one little flaw in our code.  It compiles and runs, but the output isn’t 100% perfect. See if you can spot the flaw and fix it.
+
+
 
  Old code:
  System.out.println("There'll be no green bottles, hanging on the wall");
@@ -60,12 +61,10 @@ public class BottleSong {
  1 green bottle, hanging on the wall
  And if one green bottle should accidentally fall,
  There'll be no green bottles hanging on the wall
-
  ***********************************************************************/
 
-
-/*
 package Chapter1;
+
 public class BottleSong {
     public static void main(String[] args) {
         int bottlesNum = 10;
@@ -73,22 +72,18 @@ public class BottleSong {
 
         while (bottlesNum > 0) {
 
-            if (bottlesNum == 1) {
-                word = "bottle"; // singular, as in ONE bottle.
-            }
-
             System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
             System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
-
-            if (bottlesNum == 2) {
-                System.out.println("And if one green bottle should accidentally fall,");
-            } else {
-                System.out.println("And if " + (bottlesNum - 1) + " green bottles should accidentally fall,");
-            }
-
+            System.out.println("And if one green bottle should accidentally fall,");
             bottlesNum = bottlesNum - 1;
 
-            if  (bottlesNum > 0) {
+
+            if (bottlesNum == 1) {
+                word = "bottle"; // singular, as in ONE bottle.
+                System.out.println("There'll be " + bottlesNum +
+                        " green " + word + ", hanging on the wall");
+            }
+            if (bottlesNum > 0) {
                 System.out.println("There'll be " + bottlesNum +
                         " green " + word + ", hanging on the wall");
             } else {
@@ -96,4 +91,7 @@ public class BottleSong {
             } // end else
         } // end while loop
     } // end main method
-} // end class*/
+} // end class
+
+
+
